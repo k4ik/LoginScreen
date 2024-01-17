1ea4692955d8e41531e7417bbd26ad91a6c6aff0
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:05:52 -0300

    feat: title form component

---
+++ src/components/TitleForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <p class="title">{{ title }}</p>
+</template>
+
+<script>
+export default {
+  props: ["title"]
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 22px;
+  font-weight: bold;
+  color: #1E1E1E;
+  line-height: 1%;
+}
+</style>
