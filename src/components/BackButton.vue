dce6ab9534fccf193ceaa1f73c833c2b7aa3a084
k4ik <kaikleal1234@gmail.com>
2024-01-17 18:07:09 -0300

    feat: Back Button Component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="button-container">
+        <router-link to="/"><img src="../assets/images/arrow-left.svg" alt=""></router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    .button-container {
+        background-color: #D9D9D9;
+        width: 33px;
+        height: 33px;
+        border-radius: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
