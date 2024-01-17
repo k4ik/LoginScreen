20db24998b7701dae6a6b4fde8d9d78b22fce1a1
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:04:33 -0300

    feat: create button component

---
+++ src/components/ButtonForm.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="login-btn">{{ btnText }}</button>
+</template>
+
+<script>
+export default {
+    props: ["btnText"]
+}
+</script>
+
+<style lang="scss" scoped>
+.login-btn {
+    background-color: #0FA958;
+    color: #fefefe;
+    padding: 8.5px 0;
+    border: none;
+    border-radius: 24px;
+}
+</style>
