bf8222e60fd845b4c69f8f31bc3462c6c0d706d4
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:04:47 -0300

    feat: center text component

---
+++ src/components/CenterText.vue
@@ -0,0 +1,20 @@
+<template>
+    <span class="center">{{centerText}}</span>
+</template>
+
+<script>
+export default {
+    props: ["centerText"]
+};
+</script>
+
+<style lang="scss" scoped>
+.center {
+  text-align: center;
+}
+
+.link {
+  color: #000;
+  text-decoration: underline;
+}
+</style>
