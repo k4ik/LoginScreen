92a3015d2fed914c39288d40a0b7a183e4b6e3cc
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:05:39 -0300

    feat: right text component

---
+++ src/components/RightText.vue
@@ -0,0 +1,17 @@
+<template>
+    <a class="right">Esqueceu sua senha?</a>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.right {
+    text-align: right;
+    color: #000;
+    text-decoration: underline;
+}
+</style>
