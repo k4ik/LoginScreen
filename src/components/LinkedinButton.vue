bb55663a91b902903afa8760bb7afa18e00a5437
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:05:19 -0300

    feat: linkedin button component

---
+++ src/components/LinkedinButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="linkedin-btn">
+        <img src="../assets/images/linkedin.svg" alt="">
+        Entrar com Linkedin
+    </button>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.linkedin-btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: #E0E0E0;
+    color: #1E1E1E;
+    padding: 8.5px 0;
+    border: none;
+    border-radius: 24px;
+}
+</style>
