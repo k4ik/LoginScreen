cc91370f7e63f688b271ee6b5a437cd3e789f060
k4ik <kaikleal1234@gmail.com>
2024-01-17 15:05:29 -0300

    feat: logo component

---
+++ src/components/LogoImg.vue
@@ -0,0 +1,17 @@
+<template>
+    <img src="../assets/images/fiverr.svg" alt="fiverr logo" class="logo">
+</template>
+
+<script>
+export default {
+    name: "LogoImg"
+}
+</script>
+
+<style lang="scss" scoped>
+    .logo {
+      width: 73px;
+      height: 73px;
+      margin: 0 auto 91px;
+    }
+</style>
